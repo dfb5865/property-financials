7a31c1961952df4113bba9b7ddc52f300f80360b
Dave Banan <daveba@zillow.com>
2015-06-05 15:29:16 -0700

    Added property tax

--- src/server/server.go
+++ src/server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
@@ -13,11 +14,11 @@ import (
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/API/property", GetSeedData).Queries("url", "")
+	router.HandleFunc("/API/property", GetPropertyData).Queries("url", "")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func GetSeedData(w http.ResponseWriter, r *http.Request) {
+func GetPropertyData(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 	url := params.Get("url")
 	fmt.Fprintln(w, "request url:", url)
@@ -27,22 +28,32 @@ func GetSeedData(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	doc.Find(".zsg-content-header.addr h1").Each(func(_ int, child *goquery.Selection) {
-		text := child.Text()
+	doc.Find(".zsg-content-header.addr h1").Each(func(_ int, node *goquery.Selection) {
+		text := node.Text()
 		fmt.Fprintln(w, "Address:", text)
 	})
 
-	doc.Find(".main-row.home-summary-row").Each(func(_ int, child *goquery.Selection) {
-		text := child.Text()
-		fmt.Fprintln(w, "Price:", text)
+	doc.Find(".main-row.home-summary-row").Each(func(_ int, node *goquery.Selection) {
+		text := node.Text()
+		fmt.Fprintln(w, "Price:", strings.TrimSpace(text))
 	})
 
 	hoaRegex := regexp.MustCompile(`(?i)hoa fee: \$[0-9]*\/mo`)
+	dollarAmountRegex := regexp.MustCompile(`\$[0-9]*\/mo`)
 
-	doc.Find(".fact-group-container.zsg-content-component.top-facts").Each(func(_ int, child *goquery.Selection) {
-		text := child.Text()
+	doc.Find(".fact-group-container.zsg-content-component.top-facts").Each(func(_ int, node *goquery.Selection) {
+		text := node.Text()
 		if hoaRegex.MatchString(text) {
-			fmt.Fprintln(w, "HOA:", hoaRegex.FindStringSubmatch(text))
+			fmt.Fprintln(w, "HOA:", dollarAmountRegex.FindStringSubmatch(text)[0])
+		}
+	})
+
+	doc.Find(".description.zsg-h4").Each(func(_ int, node *goquery.Selection) {
+		text := node.Text()
+		if text == "Property tax" {
+			parent := node.Parent()
+			price := parent.Find(".vendor-cost").Text()
+			fmt.Fprintln(w, "Taxes:", price)
 		}
 	})
 
