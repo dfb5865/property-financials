5d2fb20c26d3824295d45ce07b0f003cfbc57e46
Aesop Wolf <aesopwolf@gmail.com>
2015-06-04 23:03:30 -0700

    added react hot loader infrastructure

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# property-financials
+Property Financials Calculator
+==============================
+
+Visit http://example.com to play with the calculator!
+
+Development
+-----------
+
+From the root directory, run:
+
+`$ npm install
+$ npm start`
+
+This will start a server listening on your port (defined in config.js).
+
+`config.js` will be created for you automatically the first time you run `npm install`
+
+As you edit your `.jsx` files, the [React Hot Loader](http://gaearon.github.io/react-hot-loader/) will automatically update your components inside the browser without refreshing the page. This means you get to keep your existing state of your components!
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+var webpack = require('webpack');
+
+// # Create a config file from the sample one if config.js doesn't exist yet
+try {
+  fs.openSync(path.join(process.cwd(), 'config.js'), 'r');
+} catch(e) {
+  var configString = fs.readFileSync(path.join(process.cwd() + '/config-sample.js'));
+  fs.writeFileSync(path.join(process.cwd() + '/config.js'), configString);
+}
+var config = require('./config.js');
+
+// # Export all of the webpack configuration
+module.exports = {
+  port: config.port,
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  },
+};
